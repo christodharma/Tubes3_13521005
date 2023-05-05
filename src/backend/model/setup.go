d1a21192a5033af6447f2ed398607b60302ded09
kelvinra <13521005@mahasiswa.itb.ac.id>
2023-05-05 14:11:24 +0700

    feat: add frontend ,posgres database

--- src/backend/model/setup.go
+++ src/backend/model/setup.go
@@ -2,13 +2,13 @@ package model
 
 import(
 	"gorm.io/gorm"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase(){
-	var database, err = gorm.Open(mysql.Open("root:admin@tcp(localhost:3306)/ChatTMBOK"))
+	var database, err = gorm.Open(postgres.Open("postgresql://QZYpViscGxsMENNoRvYLuySFehjPQxEu:vkersMizkROgEAIFgcboXvzoLVrCnEnu@db.thin.dev/f212dad5-bb33-4d21-ab00-ca1a5b3999d1"))
 	if err != nil {
 		panic(err)
 	}
